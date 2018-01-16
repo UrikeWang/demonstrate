93c441ab9877451a5e1c790c93ee3cbfc0427a15
yuling <takusax@gmail.com>
2018-01-16 08:41:05 +0800

    main page remode slick slide

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
-(function(document, window, $){
-  $(document).ready(function(){
-      $(".slider").slick({
+$.wmBox();
+(function(document, window, $) {
+  $(document).ready(function() {
 
-        // slidesToShow: 3,
-        // slidesToScroll:3,
-        prevArrow: '.slider-container .prev',
-        nextArrow: '.slider-container .next',
-        centerPadding: '8%',
-        focusOnSelect: true,
-        slidesToShow: 4, // 1次顯示幾個選項
-        slidesToScroll: 3,
-        infinite: true,
-        // fade: true,    //圖片淡出淡入
-        // responsive: [{
-        //     breakpoint: 25,
-        //     settings: {
-        //       arrows: false,
-        //       centerMode: false, //滑動時不維持中央圖片置中
-        //     }
-        //   },
-        //   {
-        //     breakpoint: 25,
-        //     settings: {
-        //       arrows: false,
-        //       centerMode: false, //滑動時不維持中央圖片置中
-        //     }
-        //   }
-        // ]
 
-      });
+
+    $(".slider").slick({
+
+      // slidesToShow: 3,
+      // slidesToScroll:3,
+      prevArrow: '.slider-container .prev',
+      nextArrow: '.slider-container .next',
+      centerPadding: '8%',
+      focusOnSelect: true,
+      slidesToShow: 3, // 1次顯示幾個選項
+      slidesToScroll: 3,
+      infinite: true,
+      // fade: true,    //圖片淡出淡入
+      // responsive: [{
+      //     breakpoint: 25,
+      //     settings: {
+      //       arrows: false,
+      //       centerMode: false, //滑動時不維持中央圖片置中
+      //     }
+      //   },
+      //   {
+      //     breakpoint: 25,
+      //     settings: {
+      //       arrows: false,
+      //       centerMode: false, //滑動時不維持中央圖片置中
+      //     }
+      //   }
+      // ]
+
+    });
   });
 })(document, window, jQuery);
